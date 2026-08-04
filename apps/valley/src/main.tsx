58c6d9ad231f79b27ecf80fcda4715c1f3249682
interflownex <interflow.br@gmail.com>
2026-08-04 17:13:33 -0300

    style(valley): carregar camada visual da experiência consumer

--- apps/valley/src/main.tsx
+++ apps/valley/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import './assets/valley_design_system.css';
+import './experience.css';
 import App from './App.tsx';
 import { installNativeFetchBridge } from './lib/nativeBridge';
 
